23dc1d42798ba9ccbed0d50232c3cc25954f2efb
Sasha <38#mail.ru>
2024-08-06 16:53:30 +0300

    Update scriptTicTactoe.js

--- Tic-Tac-toe/js/scriptTicTactoe.js
+++ Tic-Tac-toe/js/scriptTicTactoe.js
@@ -2,10 +2,31 @@
 
 import {getValuesPadding} from './script.js'
 
+import { players } from './scriptGetName.js'
+
 let table = document.createElement('table');
 
 let tictactoe = document.getElementById('main_buttons__startId');
 table.className = 'tictactoe';
+
+let blockPlayer1 = document.createElement('div');
+blockPlayer1.style = `
+    width: 100px;
+    height: 100px;
+    background: purple;  
+`;
+let blockPlayer2 = document.createElement('div');
+blockPlayer2.style = `
+    width: 100px;
+    height: 100px;
+    background: purple;  
+`;
+
+let blockPlayersWithTicTacToe = document.createElement('div');
+blockPlayersWithTicTacToe.style = `
+    display: flex;s
+    flex-direction: row;
+`;
 
 for(let i = 0; i < 3;i++)
 {
@@ -18,7 +39,9 @@ for(let i = 0; i < 3;i++)
     }
     table.append(tr);
 }
+tictactoe.append(blockPlayer1);
 tictactoe.append(table);
+tictactoe.append(blockPlayer2);
 let valuesPadding = getValuesPadding(getComputedStyle(tictactoe));
 
 tictactoe.style.padding = `${valuesPadding[0] - 100}px ${valuesPadding[1] - 100}px`
